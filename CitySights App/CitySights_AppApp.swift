de0c11312611771e40af5e6a27a5f1db68879395
Dev Soni <iamdevsoni@gmail.com>
2025-07-29 11:38:12 +0530

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Dev Soni on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Dev Soni on 29/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
