2ad3f97ad03380ef559698b197fd456f51e52f5d
Dev Soni <iamdevsoni@gmail.com>
2025-08-07 14:00:04 +0530

    - add map markers - detect selection - set selected property of the business model

--- CitySights App/MapView.swift
+++ CitySights App/MapView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    @Environment(BusinessModel.self) var model
+    @State var selectedBusinessId: String? // becaouse the the b.id is a string
     var body: some View {
-        Map()
+        Map(selection: $selectedBusinessId) {
+            ForEach(model.businesses, id: \.id) { b in
+                Marker(b.name ?? "Restaurant", coordinate: CLLocationCoordinate2D(latitude: b.coordinates?.latitude ?? 0, longitude: b.coordinates?.longitude ?? 0))
+                    .tag(b.id ?? "None")
+            }
+        }
+        .onChange(of: selectedBusinessId) { oldValue, newValue in
+            // Find the Business which matched the id
+            let business = model.businesses.first { business in
+                business.id == newValue
+            }
+            
+            // If the Business is not nil or if the busines is found, set it as the selected one
+            if let business = business {
+                model.selectedBusiness = business
+            }
+        }
     }
 }
 
